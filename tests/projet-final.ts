a29d913cbddc74240eb79dd167b304c947214292
Spilitious <LordThergal@gmail.com>
2024-07-11 19:29:30 +0200

    test u

--- tests/projet-final.ts
+++ tests/projet-final.ts
@@ -4,41 +4,97 @@ import { Program } from "@coral-xyz/anchor";
 
 import { Keypair, Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
+
+const program = anchor.workspace.ProjetFinal as Program<ProjetFinal>;
+const connection = new Connection('http://localhost:8899', 'confirmed');
+anchor.setProvider(anchor.AnchorProvider.env());
+
+let authority =Keypair.generate();
+const wallet = Keypair.generate();
+ // Configure the client to use the local cluster.
+let bump:number =255;
+
+const mapData = Buffer.from( [1, 1, 1, 1, 1,
+  1, 0, 0, 0, 1,
+  1, 0, 0, 0, 1,
+  1, 0, 0, 0, 1,
+  1, 1 ,1 ,1, 1]); 
+const width = 5;
+const height = 5;
+
+ let gamePDA = (Keypair.generate()).publicKey;
+ 
+
+function getNextBump() {
+  bump -=1;
+  return bump;
+}
+
+function displayMapData(mapData:ArrayBuffer) {
+  console.log("Map data:");
+  for (let i = 0; i < height; i++) {
+    let line = "";
+    for (let j = 0; j < width; j++) {
+      line += mapData[i * width + j] + " ";
+    }
+    console.log(line.trim());
+  }
+
+}
+
+async function LoadFixtureForInit() {
+
+  const lamports = 10 * LAMPORTS_PER_SOL;
+  const tx = await connection.requestAirdrop(wallet.publicKey, lamports);
+  await connection.confirmTransaction(tx);
+
+  const tx3 = await connection.requestAirdrop(authority.publicKey, lamports);
+  await connection.confirmTransaction(tx3);
+  const balance2 = await connection.getBalance(wallet.publicKey);
+}
+
+async function LoadFixtureForBuild() {
+
+  LoadFixtureForInit();
+  console.log(getNextBump());
+  const lamports = 10 * LAMPORTS_PER_SOL;
+  authority =Keypair.generate();
+  const tx3 = await connection.requestAirdrop(authority.publicKey, lamports);
+  await connection.confirmTransaction(tx3);
+  const balance2 = await connection.getBalance(wallet.publicKey);
+  const seeds = [authority.publicKey.toBuffer()];
+
+    [gamePDA, bump] = await PublicKey.findProgramAddress(
+    seeds,
+    program.programId,
+    // Use a specific bump value, such as a timestamp or a counter
+  
+  );
+
+  console.log("Bump", bump)
+    
+    const tx2 = await program.rpc.initialize(width, height, mapData, {
+      accounts: {
+        game: gamePDA,
+        signer: authority.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [authority],
+    });
+
+}
+
+
+
 describe("projet-final", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+ 
+  // beforeEach(async function() {
+    // await LoadFixtureForInit();
 
-  const program = anchor.workspace.ProjetFinal as Program<ProjetFinal>;
-  const connection = new Connection('http://localhost:8899', 'confirmed');
-  
-  const authority =Keypair.generate();
-  const wallet = Keypair.generate();
-
-  
+ //});
 
   it("Is initialized!", async () => {
-
-    const lamports = 10 * LAMPORTS_PER_SOL;
-    const tx = await connection.requestAirdrop(wallet.publicKey, lamports);
-    await connection.confirmTransaction(tx);
-    
-    
-
-    const balance = await connection.getBalance(wallet.publicKey);
-    console.log("Wallet balance : ", balance);
-
-   
-    
-    const tx3 = await connection.requestAirdrop(authority.publicKey, lamports);
-    await connection.confirmTransaction(tx3);
-    const balance2 = await connection.getBalance(wallet.publicKey);
-    console.log("Signer balance : ", balance2);
-
-
-    const mapData = Buffer.from( [1, 1, 1, 1, 1, 0, 0, 1, 1,0, 0, 1, 1, 1,1 , 1]); 
-    const width = 4;
-    const height = 4;
-
+    await LoadFixtureForInit();
     // Find the PDA for the game account
     const [gamePDA, bump] = await PublicKey.findProgramAddress(
     [authority.publicKey.toBuffer()],
@@ -54,20 +110,33 @@ describe("projet-final", () => {
     });
 
 
-    // Log mapData with line breaks at each width
-    console.log("Map data:");
-    for (let i = 0; i < height; i++) {
-      let line = "";
-      for (let j = 0; j < width; j++) {
-        line += mapData[i * width + j] + " ";
-      }
-      console.log(line.trim());
-    }
+    let updatedGame = await program.account.gameState.fetch(gamePDA);
+    let updatedMapData = updatedGame.mapData;
+    displayMapData(updatedMapData);
 
-    
-    
     console.log("Ajout d'une caisse en 1-2")
-    const tx4 = await program.rpc.addItem(2,1,2,  {
+    const tx = await program.rpc.addItem(2,1,2,  {
+      accounts: {
+        game: gamePDA,
+        signer: authority.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [authority],
+    });
+
+     updatedGame = await program.account.gameState.fetch(gamePDA);
+     updatedMapData = updatedGame.mapData;
+    // Log mapData with line breaks at each width
+    displayMapData(updatedMapData);
+
+
+  });
+  
+  it("Ajout d'un élément!", async () => {
+    
+    await LoadFixtureForBuild();
+    console.log("Ajout d'une caisse en 1-2")
+    const tx = await program.rpc.addItem(2,1,2,  {
       accounts: {
         game: gamePDA,
         signer: authority.publicKey,
@@ -79,15 +148,9 @@ describe("projet-final", () => {
     const updatedGame = await program.account.gameState.fetch(gamePDA);
     const updatedMapData = updatedGame.mapData;
     // Log mapData with line breaks at each width
-    console.log("Map data:");
-    for (let i = 0; i < height; i++) {
-      let line = "";
-      for (let j = 0; j < width; j++) {
-        line += updatedMapData[i * width + j] + " ";
-      }
-      console.log(line.trim());
-    }
+    displayMapData(updatedMapData);
 
 
   });
+
 });
